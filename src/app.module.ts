944624543642e96a3710510ff77166259d35c203
Darklegion92 <onlin1992@gmail.com>
2025-06-02 10:18:28 -0500

    feat(): delete invoice

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from './config/config.module';
-import { InvoiceModule } from './invoice/invoice.module';
 import { CommonModule } from './common/common.module';
 
 @Module({
@@ -14,7 +13,6 @@ import { CommonModule } from './common/common.module';
     CommonModule,
     AuthModule,
     ConfigModule,
-    InvoiceModule,
   ],
   controllers: [],
   providers: [],
