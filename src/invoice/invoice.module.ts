36b6a5834060a2e6dee1deb02bda1129dd4e4732
Darklegion92 <onlin1992@gmail.com>
2025-06-12 11:58:44 -0500

    fix(): error rutas invoice

--- src/invoice/invoice.module.ts
+++ src/invoice/invoice.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { InvoiceService } from './domain/services/invoice.service';
 import { ConfigModule as AppConfigModule } from '../config/config.module';
 import { SharedModule } from '../shared/shared.module';
+import { InvoiceController } from './infrastructure/controllers/invoice.controller';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { SharedModule } from '../shared/shared.module';
     AppConfigModule,
     SharedModule
   ],
+  controllers: [InvoiceController],
   providers: [
     InvoiceService
   ],
